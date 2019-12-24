d7d45f02b2f9e516fcede828cde04709534b4f3d
nadjarben <nadjarbenj@gmail.com>
2019-12-24 23:57:10 +0200

    nextpwa integration

---
+++ static/precache._eFQwek2B65v_8BLo7qje.95165001c42b298f69c7d02a893d144a.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([ { "url": "/", "revision": "_eFQwek2B65v_8BLo7qje" },
+  {
+    "revision": "b1cc76d4bc733d5c261b",
+    "url": "/_next/static/chunks/3.513e66ce2cd7b30d9531.js"
+  },
+  {
+    "revision": "4740b8f505698068692c",
+    "url": "/_next/static/chunks/commons.5000067179aa7c7bf2a8.js"
+  },
+  {
+    "revision": "11cb9086908ca9c3c5de",
+    "url": "/_next/static/chunks/styles.b24b8cc79cc5e91f4aa3.js"
+  },
+  {
+    "revision": "4740b8f505698068692c",
+    "url": "/_next/static/css/commons.5cd9720f.chunk.css"
+  },
+  {
+    "revision": "11cb9086908ca9c3c5de",
+    "url": "/_next/static/css/styles.b3a0039a.chunk.css"
+  },
+  {
+    "revision": "a62d8860d4715079c03e",
+    "url": "/_next/static/runtime/main-5072d9264b8fad2e66c5.js"
+  },
+  {
+    "revision": "995ac2f804cbe4522153",
+    "url": "/_next/static/runtime/webpack-c1a42c9efda1642631ec.js"
+  },
+  {
+    "revision": "18d84e2bcf3a80f05ac5",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/_app.js"
+  },
+  {
+    "revision": "c4553a62d83305a23e8e",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/_error.js"
+  },
+  {
+    "revision": "81d7f8a3df7e226b8c3b",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/admin.js"
+  },
+  {
+    "revision": "a0389cecf88ff433dd17",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/admin/crud/[slug].js"
+  },
+  {
+    "revision": "07f61b238e40aa80e4fb",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/admin/crud/blog.js"
+  },
+  {
+    "revision": "99345ce95cbba8326e6b",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/admin/crud/blogs.js"
+  },
+  {
+    "revision": "5790ee064c1751ed43ad",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/admin/crud/category-tag.js"
+  },
+  {
+    "revision": "e3b188347ad64cd8075a",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/auth/account/activate/[id].js"
+  },
+  {
+    "revision": "f749931fc05771f3d6bf",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/auth/password/forgot.js"
+  },
+  {
+    "revision": "db66d3465344aad7fe40",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/auth/password/reset/[id].js"
+  },
+  {
+    "revision": "d681d45ccb3246e3fea1",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/blogs.js"
+  },
+  {
+    "revision": "957dbf7c90e9d2a70a20",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/blogs/[slug].js"
+  },
+  {
+    "revision": "bf4552a8474d4f848f90",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/categories/[slug].js"
+  },
+  {
+    "revision": "2a3cede0552d0fd12e43",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/contact.js"
+  },
+  {
+    "revision": "fe8a253850a4e406fb3f",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/index.js"
+  },
+  {
+    "revision": "d9f654130ba04725ca0f",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/profile/[username].js"
+  },
+  {
+    "revision": "e06eba8f609dfdb76c89",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/signin.js"
+  },
+  {
+    "revision": "0f72fcc2309dfad9ed9a",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/signup.js"
+  },
+  {
+    "revision": "cfdbe10130e3cc70f1c7",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/tags/[slug].js"
+  },
+  {
+    "revision": "abd61e935a19312e848d",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/user.js"
+  },
+  {
+    "revision": "772d96cac09eb059ffc1",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/user/crud/[slug].js"
+  },
+  {
+    "revision": "b60a4174c423d27d5ee8",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/user/crud/blog.js"
+  },
+  {
+    "revision": "61d5d96ac70742c2eacc",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/user/crud/blogs.js"
+  },
+  {
+    "revision": "f08f639f0ba26fa5dcc6",
+    "url": "/_next/static/_eFQwek2B65v_8BLo7qje/pages/user/update.js"
+  }
+]);
