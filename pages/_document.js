b4d25ef83ddbf6a951148de319540f68a483fdfa
nadjarben <nadjarbenj@gmail.com>
2019-12-24 22:55:27 +0200

    reverse last

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
-import { register, unregister } from 'next-offline/runtime'
-
 
 class MyDocument extends Document {
-
-  componentDidMount () {
-    register()
-  }
-  
   setGoogleTags() {
     if (publicRuntimeConfig.PRODUCTION) {
       return {
